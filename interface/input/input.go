0b3f1b6159b91ffe23e8342d53c3ccec88cc2ffe
Brian Starkey <stark3y@gmail.com>
2018-09-15 21:21:11 +0100

    input: Add button handler

--- interface/input/input.go
+++ interface/input/input.go
@@ -2,15 +2,35 @@ package input
 
 import (
 	"log"
+	"time"
 
 	"github.com/gvalkov/golang-evdev"
 	"github.com/usedbytes/input2"
+	"github.com/usedbytes/input2/button"
 	"github.com/usedbytes/input2/gamepad/thumbstick"
 	"github.com/usedbytes/input2/factory"
 )
 
+type Button int
+const (
+	Cross Button = iota
+	Square
+	Triangle
+	Circle
+	PS
+)
+
+type State int
+const (
+	None State = iota
+	Pressed
+	Held
+)
+
 type Collector struct {
 	leftStick, rightStick float32
+	buttons map[Button]State
+	held map[Button]bool
 }
 
 func (c *Collector) handleEvents(ch <-chan input2.InputEvent) {
@@ -27,6 +47,12 @@ func (c *Collector) handleEvents(ch <-chan input2.InputEvent) {
 			} else {
 				c.rightStick = mag
 			}
+		case button.Event:
+			if e.Value == button.Pressed {
+				c.buttons[Button(e.Keycode)] = Pressed
+			} else if e.Value == button.Held {
+				c.buttons[Button(e.Keycode)] = Held
+			}
 		}
 	}
 }
@@ -35,9 +61,16 @@ func (c *Collector) GetSticks() (float32, float32) {
 	return c.leftStick, c.rightStick
 }
 
+func (c *Collector) Buttons() map[Button]State {
+	defer func() {
+		c.buttons = make(map[Button]State)
+	}()
+	return c.buttons
+}
+
 func NewCollector() *Collector {
 	c := &Collector{
-
+		buttons: make(map[Button]State),
 	}
 
 	stopChan := make(chan bool)
@@ -48,6 +81,36 @@ func NewCollector() *Collector {
 			log.Println("Source: ", s)
 			conn := s.NewConnection()
 
+			button.MapButton(conn,
+				&button.Button{
+					Match: input2.EventMatch{evdev.EV_KEY, evdev.BTN_MODE},
+					HoldTime: (time.Millisecond * 3000),
+					Keycode: int(PS),
+				})
+			button.MapButton(conn,
+				&button.Button{
+					Match: input2.EventMatch{evdev.EV_KEY, evdev.BTN_NORTH},
+					HoldTime: (time.Millisecond * 1500),
+					Keycode: int(Triangle),
+				})
+			button.MapButton(conn,
+				&button.Button{
+					Match: input2.EventMatch{evdev.EV_KEY, evdev.BTN_EAST},
+					HoldTime: (time.Millisecond * 1500),
+					Keycode: int(Circle),
+				})
+			button.MapButton(conn,
+				&button.Button{
+					Match: input2.EventMatch{evdev.EV_KEY, evdev.BTN_SOUTH},
+					HoldTime: (time.Millisecond * 1500),
+					Keycode: int(Cross),
+				})
+			button.MapButton(conn,
+				&button.Button{
+					Match: input2.EventMatch{evdev.EV_KEY, evdev.BTN_WEST},
+					HoldTime: (time.Millisecond * 1500),
+					Keycode: int(Square),
+				})
 			thumbstick.MapThumbstick(conn,
 				&thumbstick.Thumbstick{
 					X: thumbstick.Axis{ Code: evdev.ABS_X },
