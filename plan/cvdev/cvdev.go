0837c86b84b7a5dd07c28260045d2ea469716a4d
Brian Starkey <stark3y@gmail.com>
2019-02-17 14:17:02 +0000

    plan/cvdev: Add a cvdev task
    It's just remote control with the camera running and cv.RunAlgorithm
    
    Mainly for testing how long an algorithm takes on the Pi

---
+++ plan/cvdev/cvdev.go
@@ -0,0 +1,68 @@
+// Copyright 2018 Brian Starkey <stark3y@gmail.com>
+package cvdev
+
+import (
+	"image"
+	"image/color"
+	"time"
+
+	"github.com/usedbytes/mini_mouse/bot/base"
+	"github.com/usedbytes/mini_mouse/bot/interface/input"
+	"github.com/usedbytes/mini_mouse/bot/plan"
+	"github.com/usedbytes/mini_mouse/bot/plan/rc"
+	"github.com/usedbytes/mini_mouse/cv"
+	"github.com/usedbytes/picamera"
+)
+
+const TaskName = "cvdev"
+
+type Task struct {
+	platform *base.Platform
+	lastTime time.Time
+	rcTask plan.Task
+}
+
+func (t *Task) Enter() {
+	t.platform.DisableCamera()
+	t.platform.SetCameraCrop(picamera.Rect(0.0, 0.0, 1.0, 1.0))
+	t.platform.SetCameraFormat(picamera.FORMAT_I420)
+	t.platform.Camera.SetOutSize(128, 128)
+	t.platform.EnableCamera()
+}
+
+func (t *Task) Exit() {
+	t.platform.SetVelocity(0, 0)
+	t.platform.DisableCamera()
+}
+
+func (t *Task) Tick(buttons input.ButtonState) {
+	frame, frameTime := t.platform.GetFrame()
+	if frame == nil || frameTime == t.lastTime {
+		return
+	}
+	t.lastTime = frameTime
+
+	t.rcTask.Tick(buttons)
+
+	var img image.Image
+	switch v := frame.(type) {
+	case *picamera.YCbCrFrame:
+		img = &v.YCbCr
+	default:
+		img = frame
+	}
+
+	cv.RunAlgorithm(img, nil, true)
+}
+
+func (t *Task) Color() color.Color {
+	return color.NRGBA{ 0xff, 0xff, 0xff, 0x80 }
+}
+
+func NewTask(ip *input.Collector, pl *base.Platform) *Task {
+	return &Task{
+		platform: pl,
+		rcTask: rc.NewTask(ip, pl),
+	}
+}
+
